04e18017cdf42fd7328df9e027627f4b66121b72
Shaqqour <mshaqqourm@gmail.com>
2020-12-06 18:40:07 +0200

    Gets all user input and declare all required variables

---
+++ user-input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int first;
+    int another;
+    int theNextDishTime;
+    int totalTime;
+    int numberOfDishes = 0;
+
+    cout << "Please enter the time takes to cook the first dish: ";
+    cin >> first;
+
+    cout << "\nPlease enter the time takes to cook any additional dish: ";
+    cin >> another;
+
+    cout << "\nPlease enter the total time you have for cooking: ";
+    cin >> totalTime;
+}
